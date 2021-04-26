4fa6080d8441cd554fbcaaae9caeb2f969586c37
sweiss8 <jmj.stuartweiss@gmail.com>
2021-04-25 19:23:49 -0700

    Initial Commit

---
+++ cpp_hangman/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_hangman
+//
+//  Created by Stuart Weiss on 4/25/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
